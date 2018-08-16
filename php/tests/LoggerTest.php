af0ac8beeb6d84a1370653c1a0a9b971e8d78fc7
SpotAlfa <spotalfax@gmail.com>
2018-08-16 17:44:06 +0300

    Create unit-test for Logger class

---
+++ php/tests/LoggerTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This file is a part of the "Rifmoplet" rhyme detector.
+ *
+ * @author SpotAlfa <spotalfax@gmail.com>
+ * @license MIT
+ */
+
+namespace SpotAlfa\Rifmoplet\Tests;
+
+use SpotAlfa\Rifmoplet\Logger;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests Logger.
+ *
+ * @package SpotAlfa\Rifmoplet\Tests
+ */
+class LoggerTest extends TestCase
+{
+    /**
+     * Logger instance.
+     *
+     * @var Logger
+     */
+    private $stub;
+
+    /**
+     * Creates new stub.
+     */
+    public function setUp(): void
+    {
+        $this->stub = new Logger();
+    }
+
+    /**
+     * Tests if logger generally works correct.
+     */
+    public function testLogPlainText(): void
+    {
+        $expectedOut = '[DEBUG]: Hello, World!' . PHP_EOL;
+        $debugMsg = 'Hello, World!';
+
+        $this->expectOutputString($expectedOut);
+
+        $this->stub->debug($debugMsg);
+    }
+
+    /**
+     * Tests logger string interpolation.
+     */
+    public function testLogInterpolatedText(): void
+    {
+        $expectedOut = '[DEBUG]: Hello, World!' . PHP_EOL;
+        $debugMsg = '{greetings}, {person}!';
+        $msgContext = [
+            'greetings' => 'Hello',
+            'person' => 'World'
+        ];
+
+        $this->expectOutputString($expectedOut);
+
+        $this->stub->debug($debugMsg, $msgContext);
+    }
+}
