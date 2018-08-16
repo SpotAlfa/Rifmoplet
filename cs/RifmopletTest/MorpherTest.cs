8a85a144feb9822396cb97b30ea64a7c41f34b3d
SpotAlfa <spotalfax@gmail.com>
2018-08-16 18:43:50 +0300

    Add unit-tests for Morpher class

---
+++ cs/RifmopletTest/MorpherTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rifmoplet;
+using LEMMATIZERLib;
+
+namespace RifmopletTest
+{
+    [TestClass]
+    public class MorpherTest
+    {
+        [TestMethod]
+        public void TestInitialize()
+        {
+            var lemmatizer = new LemmatizerRussian();
+            var morph = new Morpher(lemmatizer);
+
+            morph.Initialize();
+
+            Assert.AreEqual(1, lemmatizer.UseStatistic);
+        }
+
+        [TestMethod]
+        public void TestGetAccent()
+        {
+            var lemmatizer = new LemmatizerRussian();
+            var morph = new Morpher(lemmatizer);
+            morph.Initialize();
+
+            var accent = morph.GetAccent("переплетено");
+
+            Assert.AreEqual(10, accent);
+        }
+    }
+}
---
+++ cs/RifmopletTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RifmopletTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RifmopletTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("673239b9-0da0-47c1-9b6e-6308c2e31c5a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
