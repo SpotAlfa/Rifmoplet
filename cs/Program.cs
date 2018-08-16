f009ba77f8b1a5ffa8f6acd95133f2b6140d7968
SpotAlfa <spotalfax@gmail.com>
2018-08-16 17:38:40 +0300

    Create Program file

---
+++ cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LEMMATIZERLib;
+
+namespace Rifmoplet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lemmatizer = new LemmatizerRussian();
+            var morph = new Morpher(lemmatizer);
+
+            morph.Initialize();
+
+            foreach (var word in args)
+            {
+                Console.WriteLine(morph.GetAccent(word));
+            }
+        }
+    }
+}
