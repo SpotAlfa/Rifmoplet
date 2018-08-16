52bd8036136194fbd5d0668cd254b71886619429
SpotAlfa <spotalfax@gmail.com>
2018-08-16 17:43:17 +0300

    Add PHPUnit configuration

---
+++ php/tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This file is a part of the "Rifmoplet" rhyme detector.
+ *
+ * @author SpotAlfa <spotalfax@gmail.com>
+ * @license MIT
+ */
+
+namespace SpotAlfa\Rifmoplet\Tests;
+
+require __DIR__ . '/../vendor/autoload.php';
