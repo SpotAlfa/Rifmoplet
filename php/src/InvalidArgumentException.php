c8275a62b8d8ab3abe4588dcb15f0a941ea7975a
SpotAlfa <spotalfax@gmail.com>
2018-08-17 16:48:38 +0300

    Create invalid argument exception

---
+++ php/src/InvalidArgumentException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is a part of the "Rifmoplet" rhyme detector.
+ *
+ * @author SpotAlfa <spotalfa@gmail.com>
+ * @license  MIT
+ */
+
+namespace SpotAlfa\Rifmoplet;
+
+use Exception;
+
+/**
+ * Is thrown when an invalid argument passed to a function.
+ *
+ * @package SpotAlfa\Rifmoplet
+ */
+class InvalidArgumentException extends Exception
+{
+    /* Simply extends global Exception. */
+}
